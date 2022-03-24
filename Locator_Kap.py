3a618e217af4294015d2ac43b35aabee05da90ff
Katherine Newcomb <katherine@kapaga.com>
2022-03-24 14:04:10 +0000

    xxx
    xxx

--- Locator_Kap.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class MainPageLocators(object):
-    GO_BUTTON = (By.CLASS_NAME, "icon-lock-open")
-
-
-class SearchResultsPageLocators(object):
-    Search_bar = (By.ID, "ClientLoginFormLogin")
